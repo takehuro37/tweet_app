0cfebf17a8441eeb6d1b304505a3928b4d125712
takehiro37 <takehiro37@gmail.com>
2020-05-12 19:37:50 +0900

    検索機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
@@ -33,9 +33,15 @@ class TweetsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
   end
 
   def edit
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
   end
 
   private
